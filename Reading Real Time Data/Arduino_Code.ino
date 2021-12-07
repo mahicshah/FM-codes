d304a26168851f9685eb36e722fb284a4ff4b497
Mahic Shah <82862036+mahicshah@users.noreply.github.com>
2021-12-07 19:07:40 +0530

    Update Arduino_Code.ino

--- Reading Real Time Data/Arduino_Code.ino
+++ Reading Real Time Data/Arduino_Code.ino
@@ -1,12 +1,18 @@
+#include <SoftwareSerial.h>
+
 int analogPin = A0;
 int analogValue = 0;
+
+SoftwareSerial mySerial(0, 1);
 
 void setup() {
   Serial.begin(9600);
 }
 
-void loop() {analogValue = analogRead(analogPin);
-  analogValue = map(analogValue, 0, 1023, 0, 255);
+void loop() {
+  
+  analogValue = analogRead(analogPin);  //Read analog input
+  analogValue = map(analogValue, 0, 1023, 0, 255);  
   Serial.write(analogValue);
-  delay(50);
+  delay(100);
 }
